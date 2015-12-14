1211d54a797ddd14ed55af006bb18671f147d8dd
vijayyerravally <vijay.yerravally@gmail.com>
2015-12-14 16:15:07 +0100

    Update Process.java
    Added comments to help navigate through the code easily @ 12/14/2015 10:15 AM.

--- Learn/src/practice/Process.java
+++ Learn/src/practice/Process.java
@@ -14,11 +14,15 @@ public class Process {
 	}
 	       
 	public boolean search() throws IOException{
+		//Store the legally allowed Categories/keywords
 		String[] theKeywords={"PERSON","PLACE","ANIMAL","COMPUTER","OTHER"};
 		
+		// Using a Hash Map to keep track of the key-pair values
 		Map<String,Integer> Count = new HashMap<String,Integer>();
+		//To process only the values in the 'theKeywords' array
 		for (String str : theKeywords)
 		{
+			//Initializing the counter to 0
 			Count.put(str, 0);
 		}
 		
@@ -32,15 +36,21 @@ public class Process {
 			}
 			
 			else {
+				//process line by line till end of the file
 			while ((sCurrentLine = br.readLine()) !=null)
 			{
+				//Getting number of words in the line, using space as a delimiter
 				int Categorylength=sCurrentLine.split(" ").length;
 				
 				String[] Category = sCurrentLine.split(" ");
+				//since we do not have to count lines where category does not have a value
+				//checking for lines that more than one word, meaning lines having category and its value
 				if (Categorylength !=1)
 				{
 					if (Count.containsKey(Category[0]))
 					{
+						//getting the Category which is first word in the line and keeping 
+						//track of its count
 						Count.put(Category[0], Count.get(Category[0])+1);
 					}
 				}
@@ -55,19 +65,19 @@ public class Process {
 		}
 				catch (FileNotFoundException exception)
 				{
+					//Throwing an error on to the console if file is not available
 					System.out.println("Please provide an input file for processing. Provide the below message to the System Administrator");
 					exception.printStackTrace();
 					return false;
 				}
+		//Printing the header line		
 		System.out.printf("%10s %10s\n","Category", "Count");
 		for(String Category : theKeywords){
+			//Printing the Category and the final Count
 			System.out.printf("%10s %10s\n",Category,Count.get(Category));
 		}
 		return true;
 	}
-
-	//public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
